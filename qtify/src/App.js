9083541887ec40acbe7b213f4a723a5adf41f8c6
gogul07 <goguldeveloper@gmail.com>
2024-04-05 00:20:49 +0530

    Test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/Header/Header";
+import GridSection from "./components/GridSection/GridSection";
+// api
+import { fetchNewAlbums, fetchTopAlbums, fetchSongs } from "./api/api";
 function App() {
+  // 1)
+  const [dataTopAlbums, setDataTopAlbums] = useState([]);
+  // const [dataNewAlbum, setDataNewAlbum] = useState([]);
+  // const [dataSong, setDataSong] = useState([]);
+  // 2)
+  const getDataTopAlbums = async () => {
+    try {
+      const res = await fetchTopAlbums();
+      setDataTopAlbums(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // 3)
+  useEffect(() => {
+    getDataTopAlbums();
+    // getDataNewAlbums();
+    // getDataSongs()
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
       <Header />
+      <GridSection title={"Top Albums"} data={dataTopAlbums} type={"album"} />
     </div>
   );
 }
