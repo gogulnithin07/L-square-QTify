6ddb23007caf54577570a9dc2603c5d7cc4a3bba
gogul07 <goguldeveloper@gmail.com>
2024-04-04 20:08:06 +0530

    Test commit

---
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+// import { Logo, Modals, Search1, SearchBar } from '../../components';
+import React from "react";
+import Modals from "../Modals/Modals";
+// import { Logo, Search1 } from "../../components";
+import Logo from "../Logo/Logo";
+import Search1 from "../SearchBar/Search1";
+// import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const NavBar = ({ data }) => {
+  return (
+    <>
+      <nav className="navbar">
+        {/* <Link to="/">
+          <Logo />
+        </Link> */}
+        <Logo />
+        {/* <SearchBar data={data} className="searchbar" /> */}
+        <Search1 data={data} className="searchbar" />
+        <Modals title={"Give Feedback"} />
+      </nav>
+      {/* <SearchBar className="searchbar-moblie" /> */}
+      <Search1 data={data} className="searchbar-moblie" />
+    </>
+  );
+};
+
+export default NavBar;
